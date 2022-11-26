29013b4e9b041d8adfd3f1b9fa85232bc48ac7b7
Julien Wolfensberger <jwolfensb@ethz.ch>
2022-11-26 14:59:56 +0100

    wrap cluster builder in try.. except

--- source/preprocessing/clustering/cluster_feature_builder.py
+++ source/preprocessing/clustering/cluster_feature_builder.py
@@ -11,6 +11,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import umap
 import pandas as pd
+import sys
 
 
 
@@ -21,37 +22,40 @@ class ClusterFeatureBuilder(object):
         
         if Constants.VERBOSE:
             print("Predicting clusters...")
+        try:
         
-        # TODO: I need to implement this in a cleaner way as to avoid making mistakes
-        features_df = DataFrameLoader.load_feature_dataframe(subject_id, session_id, ClusteringFeatureService.cluster_feature_types, dataset)
-        data = features_df.to_numpy().squeeze()
-        features = data[:,1:].squeeze()
-        timestamps = data[:,0].squeeze()
-        
-        clusters = clustering_model.predict(features)
-        
-        
-        ### PLOTTING
-        
-        if Constants.MAKE_PLOTS_PREPROCESSING and dataset.name == DataSet.usi.name:
-            plt.scatter((timestamps - timestamps[0])/3600, clusters, color=cm.cool(30*np.abs(clusters)), edgecolors='none')
-            plt.savefig(str(Constants.FIGURE_FILE_PATH) + "/clusters/" + subject_id + "_" + session_id)
-            plt.clf()
+            # TODO: I need to implement this in a cleaner way as to avoid making mistakes
+            features_df = DataFrameLoader.load_feature_dataframe(subject_id, session_id, ClusteringFeatureService.cluster_feature_types, dataset)
+            data = features_df.to_numpy().squeeze()
+            features = data[:,1:].squeeze()
+            timestamps = data[:,0].squeeze()
             
-            if(features.shape[0] > 20):
-                reducer = umap.UMAP()
-                embedding = reducer.fit_transform(features)
-                plt.scatter(embedding[:, 0], embedding[:, 1], c=clusters, cmap='Spectral', s=8)
-                plt.savefig(str(Constants.FIGURE_FILE_PATH) + "/umap/" + subject_id + "_" + session_id)
+            clusters = clustering_model.predict(features)
+            
+            
+            ### PLOTTING
+            
+            if Constants.MAKE_PLOTS_PREPROCESSING and dataset.name == DataSet.usi.name:
+                plt.scatter((timestamps - timestamps[0])/3600, clusters, color=cm.cool(30*np.abs(clusters)), edgecolors='none')
+                plt.savefig(str(Constants.FIGURE_FILE_PATH) + "/clusters/" + subject_id + "_" + session_id)
                 plt.clf()
-            
-            
+                
+                if(features.shape[0] > 20):
+                    reducer = umap.UMAP()
+                    embedding = reducer.fit_transform(features)
+                    plt.scatter(embedding[:, 0], embedding[:, 1], c=clusters, cmap='Spectral', s=8)
+                    plt.savefig(str(Constants.FIGURE_FILE_PATH) + "/umap/" + subject_id + "_" + session_id)
+                    plt.clf()
+                
+                
+    
+            # Writing all features to their files
+            timestamped_clusters = np.stack([timestamps, clusters], axis=1)
+            clusters_df = pd.DataFrame(timestamped_clusters)
+            clusters_df.columns = ["epoch_timestamp", "cluster"]
+            DataWriter.write_epoched(clusters_df, subject_id, session_id, FeatureType.epoched_cluster, dataset)
+        except:
+            print("Error: ", sys.exc_info()[0], " while building clusters for " + str(subject_id), ", session " + str(session_id))
 
-        # Writing all features to their files
-        timestamped_clusters = np.stack([timestamps, clusters], axis=1)
-        clusters_df = pd.DataFrame(timestamped_clusters)
-        clusters_df.columns = ["epoch_timestamp", "cluster"]
-        DataWriter.write_epoched(clusters_df, subject_id, session_id, FeatureType.epoched_cluster, dataset)
-                                     
 
         
