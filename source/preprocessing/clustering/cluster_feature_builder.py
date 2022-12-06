17c360f668e24e2c11f3e795d165dcdbcd4584cc
Julien Wolfensberger <jwolfensb@ethz.ch>
2022-12-06 10:20:21 +0100

    another try.. except block

--- source/preprocessing/clustering/cluster_feature_builder.py
+++ source/preprocessing/clustering/cluster_feature_builder.py
@@ -27,30 +27,34 @@ class ClusterFeatureBuilder(object):
         
         with tqdm(subject_sleepsession_dictionary.keys(), unit='subject', colour='green') as t:
             for subject_id in t:
-                t.set_description("Building " + dataset.name.upper() + " Cluster Features")
-                
-                subject_df = DataFrameLoader.load_feature_dataframe(subject_id, RunnerParameters.CLUSTERING_FEATURES, dataset)
-                subject_mean = np.mean(subject_df.iloc[:,1:], axis=0)
-                subject_std = np.std(subject_df.iloc[:,1:], axis=0)
-                
-                for session_id in subject_sleepsession_dictionary[subject_id]:
+                try:
+                    t.set_description("Building " + dataset.name.upper() + " Cluster Features")
                     
-                    try:
-                        session_df = DataFrameLoader.load_feature_dataframe(subject_id, session_id, RunnerParameters.CLUSTERING_FEATURES, dataset)
-                        normalized_session_df = session_df
+                    subject_df = DataFrameLoader.load_feature_dataframe(subject_id, RunnerParameters.CLUSTERING_FEATURES, dataset)
+                    subject_mean = np.mean(subject_df.iloc[:,1:], axis=0)
+                    subject_std = np.std(subject_df.iloc[:,1:], axis=0)
+                    
+                    for session_id in subject_sleepsession_dictionary[subject_id]:
                         
-                        if RunnerParameters.CLUSTERING_PER_SUBJECT_NORMALIZATION:
-                            normalized_session_df.iloc[:,1:] = (normalized_session_df.iloc[:,1:] - subject_mean)/subject_std
-                            normalized_session_df[['count']] = normalized_session_df[['count']] + (subject_mean['count']/subject_std['count'])
-                        else:
-                            normalized_session_df.iloc[:,1:] = (normalized_session_df.iloc[:,1:] - overall_mean)/overall_std
-                            normalized_session_df[['count']] = normalized_session_df[['count']] + (overall_mean['count']/overall_std['count'])
-                       
-                        PathService.create_clusters_folder_path(subject_id, session_id, dataset)
-                        DataWriter.write_cluster(normalized_session_df, subject_id, session_id, FeatureType.cluster_features, dataset)
-                    except:
-                        ExceptionLogger.append_exception(subject_id, session_id, "Cluster Features", dataset.name, sys.exc_info()[0])
-                        print("Error: ", sys.exc_info()[0], " while building cluster features for " + str(subject_id), ", session " + str(session_id))
-        
+                        try:
+                            session_df = DataFrameLoader.load_feature_dataframe(subject_id, session_id, RunnerParameters.CLUSTERING_FEATURES, dataset)
+                            normalized_session_df = session_df
+                            
+                            if RunnerParameters.CLUSTERING_PER_SUBJECT_NORMALIZATION:
+                                normalized_session_df.iloc[:,1:] = (normalized_session_df.iloc[:,1:] - subject_mean)/subject_std
+                                normalized_session_df[['count']] = normalized_session_df[['count']] + (subject_mean['count']/subject_std['count'])
+                            else:
+                                normalized_session_df.iloc[:,1:] = (normalized_session_df.iloc[:,1:] - overall_mean)/overall_std
+                                normalized_session_df[['count']] = normalized_session_df[['count']] + (overall_mean['count']/overall_std['count'])
+                           
+                            PathService.create_clusters_folder_path(subject_id, session_id, dataset)
+                            DataWriter.write_cluster(normalized_session_df, subject_id, session_id, FeatureType.cluster_features, dataset)
+                        except:
+                            ExceptionLogger.append_exception(subject_id, session_id, "Cluster Features", dataset.name, sys.exc_info()[0])
+                            print("Error: ", sys.exc_info()[0], " while building cluster features for " + str(subject_id), ", session " + str(session_id))
+                except:
+                    ExceptionLogger.append_exception(subject_id, "Cluster Features", dataset.name, sys.exc_info()[0])
+                    print("Error: ", sys.exc_info()[0], " while building cluster features for " + str(subject_id))
+                    
 
     
